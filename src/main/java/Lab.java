788497b8895c4669c0f13eba9384ce93044219bd
Charles V <charlesvaldezz94@gmail.com>
2023-02-15 23:42:55 +0000

    passed all tests

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object Joy = new Object();
+        return Joy;
     }
 }
